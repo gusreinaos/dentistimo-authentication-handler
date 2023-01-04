28e74ceb8f3c7f48d798ac247a5c1d0cf2e818e2
Oscar Reina Gustafsson <90770844+gusreinaos@users.noreply.github.com>
2023-01-04 16:30:25 +0100

    fix: sign in issue

---
+++ src/Domain/Errors/UnerNotAuthenticatedError.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserNotAuthenticatedError = void 0;
+class UserNotAuthenticatedError {
+}
+exports.UserNotAuthenticatedError = UserNotAuthenticatedError;
+UserNotAuthenticatedError.code = 'user_not_authenticated_error';
+UserNotAuthenticatedError.detail = 'User not authenticated';
